ec8c9ea4f2f822d41cacc4d16bc527e72026e329
Anthony Ross <aross@brex.com>
2024-05-08 16:42:39 -0700

    Lua file

---
+++ init.lua
@@ -0,0 +1,162 @@
+
+vim.g.mapleader = ','
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = "," -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+local elixir_tools = {
+  "elixir-tools/elixir-tools.nvim",
+  version = "*",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local elixir = require("elixir")
+    local elixirls = require("elixir.elixirls")
+
+    elixir.setup {
+      nextls = {enable = true},
+      credo = {},
+      elixirls = {
+        enable = true,
+        settings = elixirls.settings {
+          dialyzerEnabled = false,
+          enableTestLenses = false,
+        },
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+        end,
+      }
+    }
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { 
+	  "folke/neoconf.nvim", 
+	  cmd = "Neoconf" 
+  },
+  "folke/neodev.nvim",
+  "neovim/nvim-lspconfig",
+  'tpope/vim-fugitive',
+  {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+	  'numToStr/Comment.nvim',
+	  opts = {
+		  -- add any options here
+	  },
+	  lazy = false,
+  },
+  {
+	  -- add dracula
+	  { "Mofiqul/dracula.nvim" },
+
+	  -- Configure LazyVim to load dracula
+	  {
+		  "LazyVim/LazyVim",
+		  opts = {
+			  colorscheme = "dracula",
+		  },
+	  },
+  },
+  "github/copilot.vim",
+  "nvim-treesitter/nvim-treesitter",
+
+})
+
+-- require'lspconfig'.kotlin_language_server.setup{}
+local nvim_lsp = require('lspconfig')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { "kotlin_language_server", "pyright", "rust_analyzer", "tsserver" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+
+require'lspconfig'.elixirls.setup{
+	on_attach = on_attach,
+	cmd = { "/Users/aross/projects/elixir-ls/language_server.sh" }
+}
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fgt', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.api.nvim_set_keymap('n', '<leader>fw', [[<Cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') })<CR>]], { noremap = true, silent = true })
+
+
+require("Comment").setup()
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all", -- install parsers for all supported languages
+  sync_install = false,
+  auto_install = true,
+  ignore_install = { },
+  highlight = {
+    enable = true,
+    disable = { },
+  },
+}
